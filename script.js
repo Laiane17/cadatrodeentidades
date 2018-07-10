6c041f712cde3beab71bce75f558c55b39d11e45
Laiane <Eulaianesilva@gmail.com>
2018-07-10 00:53:56 -0300

    Alterações no CSS e HTML

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector(".lista");
+const botao = document.querySelector("#hamburguer");
+const nav = document.querySelector("nav");
+
+
+
+
+function acaobotao(){
+  if(menu.classList.contains("active")){
+    menu.classList.remove("active");
+    nav.classList.remove("active");
+  }
+  else{
+    menu.classList.add("active");
+    nav.classList.add("active");
+  }
+
+}
+
+botao.onclick = acaobotao;
